a014b8af3d28e638c1480f1172d8651b85c620da
Craig Dunlop <craigdun9393@gmail.com>
2018-09-29 12:24:14 +0100

    created update function on merchants

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,22 @@
 require_relative("../models/merchant")
 require("pry-byebug")
 
+Merchant.delete_all()
+
 merchant1 = Merchant.new({
   "name" => "Amazon"
   })
   merchant1.save()
 
+  merchant2 = Merchant.new({
+    "name" => "Tesco"
+    })
+    merchant2.save()
+
+    merchant3 = Merchant.new({
+      "name" => "Spa Town Coffee"
+      })
+      merchant3.save()
+
 binding.pry
-nil 
+nil
--- models/merchant.rb
+++ models/merchant.rb
@@ -1,6 +1,10 @@
 require_relative('../db/sql_runner')
 
 class Merchant
+
+  attr_accessor :name
+  attr_reader :id
+  
   def initialize(options)
     @id = options["id"].to_i if options["id"]
     @name = options["name"]
@@ -13,9 +17,20 @@ class Merchant
     @id = result.first["id"].to_i
   end
 
+  def update()
+    sql = "UPDATE merchants SET name = $1 WHERE id = $2"
+    values = [@name, @id]
+    SqlRunner.run(sql,values)
+  end
+
   def delete()
     sql = "DELETE FROM merchants WHERE id = $1"
     values = [@id]
     SqlRunner.run(sql,values)
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM merchants"
+    SqlRunner.run(sql)
   end
 end
