5311b544db2dc8c6e62e7017cdede76aa7ff03a9
Craig Dunlop <craigdun9393@gmail.com>
2018-09-29 12:10:17 +0100

    created seeds, merchant and save function

---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+require_relative("../models/merchant")
+
+merchant1 = Merchant.new({
+  "name" => "Amazon"
+  })
+  merchant1.save()
---
+++ models/merchant.rb
@@ -0,0 +1,15 @@
+require_relative('../db/sql_runner')
+
+class Merchant
+  def initialize(options)
+    @id = options["id"].to_i if options["id"]
+    @name = options["name"]
+  end
+
+  def save()
+    sql = "INSERT INTO merchants (name) VALUES ($1) RETURNING id"
+    values = [@name]
+    result = SqlRunner.run(sql,values)
+    @id = result.first["id"].to_i
+  end
+end
