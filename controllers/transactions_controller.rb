151fbb1ebee5976f2dca9f2d36cca47b37aab0a9
Craig Dunlop <craigdun9393@gmail.com>
2018-09-29 19:06:41 +0100

    rearranged controllers to seperate folders and created a main controller file

--- controllers/transactions_controller.rb
+++ controllers/transactions_controller.rb
@@ -1,19 +1,19 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
-require_relative("./models/merchant")
-require_relative("./models/tag")
-require_relative("./models/transaction")
-also_reload('./models/*')
+require_relative("../models/merchant")
+require_relative("../models/tag")
+require_relative("../models/transaction")
+also_reload('../models/*')
 
 get'/transactions' do
   @transactions = Transaction.all()
-  erb(:index)
+  erb(:"transactions/index")
 end
 
 get'/transactions/new' do
   @merchants = Merchant.all()
   @tags = Tag.all()
-  erb(:new)
+  erb(:"transactions/new")
 end
 
 post'/transactions' do
